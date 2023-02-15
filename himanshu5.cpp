e186bc85c104ac23b6d2b8008501b6fc6cc52781
himanshumishracs22 <125138818+himanshumishracs22@users.noreply.github.com>
2023-02-15 10:12:42 +0530

    Add files via upload

---
+++ himanshu5.cpp
@@ -0,0 +1,10 @@
+//program no.2
+#include <stdio.h>
+int main(){
+	int a,b,sum;
+	printf("enter two  numbers");
+	scanf("%d %d",&a,&b);
+	sum=a+b;
+	printf("a=%d\nb=%d\nsum=%d\n",a,b,sum);
+	return 0;
+}
