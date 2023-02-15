6b9c808ed5e832fe514c8cb89d2f4d8313e5a574
himanshumishracs22 <125138818+himanshumishracs22@users.noreply.github.com>
2023-02-15 10:13:09 +0530

    Add files via upload

---
+++ himanshu4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	int length,width,area;
+	printf("enter length of rectangle");
+	scanf("%d",&length);
+	printf("enter width of rectangle");
+	scanf("%d",&width);
+	area=length*width;
+	printf("area of  rectangle=%d ",area);
+	return 0;
+}
