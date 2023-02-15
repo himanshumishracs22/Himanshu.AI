5e4fa66f4beefbb5da2386ee07a2f18ac17fa84d
himanshumishracs22 <125138818+himanshumishracs22@users.noreply.github.com>
2023-02-15 10:06:01 +0530

    Add files via upload

---
+++ himanshu.cpp
@@ -0,0 +1,9 @@
+//program no.1
+#include <stdio.h>
+int main(){
+	printf("Name   :Himanshu Mishra\n");
+	printf("  :B.tech\n");
+	printf("branch :CS(AIML)\n");class
+	printf("Roll no:38\n");
+	return 0;
+}
