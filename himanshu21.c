5334ceb0e6b15c7f14ce5fefecc5c9ef72eff4f3
himanshumishracs22 <125138818+himanshumishracs22@users.noreply.github.com>
2023-03-01 11:04:13 +0530

    Add files via upload

---
+++ himanshu21.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+	int year;
+	printf("enter any year");
+	scanf("%d",&year);
+	(year%4==0&&year%100!=0)? printf("LEAP YEAR"):(year%400==0)?printf("LEAP YEAR"):printf("COMMON YEAR");
+	return 0;
+}
