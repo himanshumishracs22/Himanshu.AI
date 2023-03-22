f99511e84f99f61d938bcf252c6619bf6772bfd7
himanshumishracs22 <125138818+himanshumishracs22@users.noreply.github.com>
2023-03-22 10:10:31 +0530

    Add files via upload

---
+++ switch case que3.c
@@ -0,0 +1,45 @@
+//to check whether an alphabet is vowel or consonant usimg switch case
+#include<stdio.h>
+int main(){
+	char ch;
+	printf("enter any alphabet:");
+	scanf("%c",&ch);
+	switch(ch)
+	{
+		case 'a':
+			printf("vowel");
+			break;
+		case 'e':
+		    printf("vowel");
+		    break;
+		case 'i':
+		    printf("vowel");
+		    break;
+		case'o':
+		    printf("vowel");
+		    break;
+		case 'u':
+		    printf("vowel");
+		    break;
+		case 'A':
+			printf("vowel");
+			break;
+		case 'E':
+		    printf("vowel");
+		    break;
+		case 'I':
+		    printf("vowel");
+		    break;
+		case'O':
+		    printf("vowel");
+		    break;
+		case 'U':
+		    printf("vowel");
+		    break;
+		default:
+			printf("consonant");
+				
+}					
+	return 0;
+}
+ 
